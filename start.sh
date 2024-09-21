7a1ea826933a64bf46b03a14c156f33f53b35ec1
Ông Tùng Dương <ongtungduong@gmail.com>
2024-09-21 22:11:34 +0700

    update code

--- start.sh
+++ start.sh
@@ -3,13 +3,18 @@
 if [ -f .env ]; then
     echo ".env already exists"
 else
-    RABBITMQ_DEFAULT_PASS=$(openssl rand -base64 32 | tr -d 'iI1lLoO0' | tr -d -c '[:alnum:]' | cut -c1-32)
+    RABBITMQ_PASSWORD=$(openssl rand -base64 32 | tr -d 'iI1lLoO0' | tr -d -c '[:alnum:]' | cut -c1-32)
     cat << EOF > .env
-RABBITMQ_DEFAULT_USER=admin
-RABBITMQ_DEFAULT_PASS=$RABBITMQ_DEFAULT_PASS
-RABBITMQ_DEFAULT_VHOST=/
+RABBITMQ_USER=admin
+RABBITMQ_PASSWORD=$RABBITMQ_PASSWORD
+RABBITMQ_VHOST=/
 EOF
 fi
+
+cat << EOF >> .env
+RABBITMQ_VERSION=4.0.1-management
+HAPROXY_VERSION=2.8.11
+EOF
 
 if [ -f .erlang.cookie ]; then
     echo ".erlang.cookie already exists"
@@ -17,7 +22,6 @@ else
     ERLANG_COOKIE=$(openssl rand -base64 32 | tr -d 'iI1lLoO0' | tr -d -c '[:alnum:]' | cut -c1-32)
     echo $ERLANG_COOKIE > .erlang.cookie
 fi
-
 
 docker compose up -d
 
